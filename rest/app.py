4384f45b452d602f79af21ae5aaf18ccc9a66357
Mark McDowall <mark.mcdowall@gmail.com>
2016-09-02 16:33:34 +0100

    Initial functionality of the RESTful API for retrieving predicted TADs

---
+++ rest/app.py
@@ -0,0 +1,282 @@
+from flask import Flask, make_response, request
+from flask_restful import Api, Resource
+
+from datasets import datasets
+from cassandra_reader import cassandra_reader
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.representation('application/tsv')
+def output_tsv(data, code, headers=None):
+    """
+    TSV representation for interactions
+    """
+    if request.endpoint == "values":
+        outstr = ''
+        for v in data["values"]:
+            outstr += str(v["chrA"]) + "\t" + str(v["startA"]) + "\t" + str(v["chrB"]) + "\t" + str(v["startB"]) + "\t" + str(v["value"]) + "\n"
+        resp = make_response(outstr, code)
+        resp.headers.extend(headers or {})
+        return resp
+
+
+class GetChrParam(Resource):
+    """
+    Class to handle the http requests for retrieving all chr_params
+    """
+    
+    def get(self):
+        ds = datasets()
+        return ds.getChr_param()
+
+
+class GetTaxons(Resource):
+    """
+    Class to handle the http requests for retrieving a list of taxon IDs
+    """
+    
+    def get(self):
+        ds = datasets()
+        taxons = ds.getTaxon()
+        request_path = request.path
+        rp = request_path.split("/")
+        return {
+            '_links': {
+                'self': request.base_url,
+                'child': [request.base_url + "/" + str(d) for d in taxons]
+            },
+            'taxons': taxons
+        }
+
+
+class GetAccessions(Resource):
+    """
+    Class to handle the http requests for retrieving a list of accessions for a 
+    given taxonic ID
+    """
+    
+    def get(self, taxon_id):
+        ds = datasets()
+        accessions = ds.getAccessions(taxon_id)
+        request_path = request.path
+        rp = request_path.split("/")
+        return {
+            '_links': {
+                'self': request.base_url,
+                'child': [request.base_url + "/" + str(d) for d in accessions],
+                'parent': request.url_root + 'rest/' + str(rp[2]) + '/' + str(rp[3]) + '/' + str(taxon_id)
+            },
+            'accessions': accessions
+        }
+
+class GetAccession(Resource):
+    """
+    Class to handle the http requests for retrieving a list of accessions for a 
+    given taxonic ID
+    """
+    
+    def get(self, taxon_id, accession_id):
+        #ds = datasets()
+        #accessions = ds.getAccessions(taxon_id)
+        request_path = request.path
+        rp = request_path.split("/")
+        return {
+            '_links': {
+                'self': request.base_url,
+                'child': [
+                    request.base_url + "/dataset",
+                    request.base_url + "/experiments",
+                    request.base_url + "/resolutions",
+                    request.base_url + "/chromosomes",
+                    request.base_url + "/tads"
+                ],
+                'parent': request.url_root + 'rest/' + str(rp[2]) + '/' + str(rp[3]) + '/' + str(taxon_id)
+            },
+            'accession': accession_id
+        }
+
+class GetDatasets(Resource):
+    """
+    Class to handle the http requests for retrieving a list of datasets for a
+    given accession
+    """
+    
+    def get(self, taxon_id, accession_id):
+        ds = datasets()
+        dataset = ds.getDatasets(taxon_id, accession_id)
+        request_path = request.path
+        rp = request_path.split("/")
+        return {
+            '_links': {
+                'self': request.base_url,
+                'child': [request.base_url + "/" + str(d) for d in dataset],
+                'parent': request.url_root + 'rest/' + str(rp[2]) + '/' + str(rp[3]) + '/' + str(taxon_id) + '/' + str(accession_id)
+            },
+            'datasets': dataset
+        }
+
+class GetExperiments(Resource):
+    """
+    Class to handle the http requests for retrieving a list of datasets for a
+    given accession
+    """
+    
+    def get(self, taxon_id, accession_id):
+        ds = datasets()
+        dataset = ds.getDatasets(taxon_id, accession_id)
+        request_path = request.path
+        rp = request_path.split("/")
+        return {
+            '_links': {
+                'self': request.base_url,
+                'child': [request.base_url + "/" + str(d) for d in dataset],
+                'parent': request.url_root + 'rest/' + str(rp[2]) + '/' + str(rp[3]) + '/' + str(taxon_id) + '/' + str(accession_id)
+            },
+            'datasets': dataset
+        }
+
+class GetResolutions(Resource):
+    """
+    Class to handle the http requests for retrieving the available resolutions
+    that have been loaded in a dataset
+    """
+    
+    def get(self, taxon_id, accession_id):
+        ds = datasets()
+        resolutions = ds.getResolutions()
+        request_path = request.path
+        rp = request_path.split("/")
+        return {
+            '_links': {
+                'self': request.base_url,
+                'child': [request.base_url + "/" + str(r) for r in resolutions],
+                'parent': request.url_root + 'rest/' + str(rp[2]) + '/' + str(rp[3]) + '/' + str(taxon_id) + '/' + str(accession_id)
+            },
+            'resolutionss': resolutions
+        }
+
+
+class GetChromosomes(Resource):
+    """
+    Class to handle the http requests for retrieving the list of chromosomes for
+    a given accession
+    """
+    
+    def get(self, taxon_id, accession_id):
+        ds = datasets()
+        resolutions = ds.getResolutions()
+        chr_param = ds.getChromosomes(taxon_id, accession_id, resolutions[-1])
+        request_path = request.path
+        rp = request_path.split("/")
+        children = []
+        chromosomes = []
+        for c in chr_param:
+          children.append(request.base_url + "/" + c[0])
+          chromosomes.append(c[0])
+        return {
+            '_links': {
+                'self': request.base_url,
+                'child': children,
+                'parent': request.url_root + 'rest/' + str(rp[2]) + '/' + str(rp[3]) + '/' + str(taxon_id) + '/' + str(accession_id)
+            },
+            'chromosomes': chromosomes
+        }
+
+class GetTADs(Resource):
+    """
+    Class to handle the http requests for retrieving ranges of TADs from
+    a given dataset
+    """
+    
+    def get(self, taxon_id, accession_id):
+        ds = datasets()
+        resolutions = ds.getResolutions()
+        chromos = ds.getChromosomes(taxon_id, accession_id, resolutions[-1])
+        chromosomes = [i[0] for i in chromos]
+        
+        error = False
+        
+        chr_id = request.args.get('chr')
+        start = int(request.args.get('start'))
+        end = int(request.args.get('end'))
+        resolution = int(request.args.get('res'))
+        
+        if chr_id not in chromosomes:
+            return {
+                "error" : "No chr parameter provided"
+            }
+        
+        if resolution == None:
+            return {
+                "error" : "No res parameter provided"
+            }
+        
+        if start == None or end == None:
+            return {
+                "error" : "No start and/or end parameter provided"
+            }
+        
+        #return {
+        #    "chr"         : chr_id,
+        #    "start"       : start,
+        #    "end"         : end,
+        #    "resolution"  : resolution
+        #}
+        
+        request_path = request.path
+        rp = request_path.split("/")
+        value_url = request.url_root + 'rest/' + str(rp[2]) + '/getValue/' + str(taxon_id)
+        
+        # Get the TADs
+        cr = cassandra_reader()
+        cr.get_range(accession_id, resolution, chr_id, start, end)
+        
+        #app.logger.warn(x["log"])
+        
+        return {
+            '_links': {
+                'self': request.url,
+                'parent': request.url_root + '/rest/' + str(rp[2]) + '/' + str(rp[3]) + '/' + str(taxon_id) + '/' + str(accession_id) + '/' + str(dataset) + '/' + str(resolution) + "/" + str(chr_id)
+            },
+            'resolution': resolution,
+            'genome': accession_id,
+            'tad_count': len(x["results"]),
+            'values': x["results"],
+            'log': x["log"]
+        }
+
+
+"""
+Define the URIs and their matching methods
+"""
+#   List the available species for which there are datasets available
+api.add_resource(GetTaxons, "/rest/v0.0/getTADs", endpoint='taxons')
+
+#   List the available assemblies for a given species with links
+api.add_resource(GetAccessions, "/rest/v0.0/getTADs/<string:taxon_id>", endpoint='accessions')
+
+#   List the available available options for an accession
+api.add_resource(GetAccession, "/rest/v0.0/getTADs/<string:taxon_id>/<string:accession_id>", endpoint='accession')
+
+#   List the available datasets for a given genome with links
+api.add_resource(GetDatasets, "/rest/v0.0/getTADs/<string:taxon_id>/<string:accession_id>/datasets", endpoint='datasets')
+
+#   List the available experiments for a given dataset with links
+api.add_resource(GetExperiments, "/rest/v0.0/getTADs/<string:taxon_id>/<string:accession_id>/experiments", endpoint='experiments')
+
+#   List the resolutions available with links
+api.add_resource(GetResolutions, "/rest/v0.0/getTADs/<string:taxon_id>/<string:accession_id>/resolutions", endpoint='resolutions')
+
+#   List the Chromosomes and their sizes and number of bins for the given resolution with links
+api.add_resource(GetChromosomes, "/rest/v0.0/getTADs/<string:taxon_id>/<string:accession_id>/chromosomes", endpoint='chromosomes')
+
+#   List the TADs for a given region
+api.add_resource(GetTADs, "/rest/v0.0/getTADs/<string:taxon_id>/<string:accession_id>/tads", endpoint='values')
+
+
+"""
+Initialise the server
+"""
+if __name__ == "__main__":
+    app.run()
---
+++ rest/cassandra_reader.py
@@ -0,0 +1,40 @@
+#import numpy as np
+import json
+from cassandra.cluster import Cluster
+
+class cassandra_reader:
+    """
+    Class related to handling the functions for interacting directly with the
+    Cassandra db. All required information should be passed to this class.
+    """
+    
+    def __init__(self):
+        """
+        Intialisation function for the datasets class
+        """
+        
+        # Open database connection
+        cluster = Cluster()
+        self.session = cluster.connect('tad_regions')
+    
+    def get_range(self, accession_id, resolution, chr_id, start, end, value_url = '/rest/v0.0/getValue/9606'):
+        """
+        Get the interactions that happen within a defined region on a specific
+        chromosome. Returns inter and intra interactions with the defined region.
+        """
+        
+        #sql = "SELECT chromosome, start, end, dataset, experiment FROM tads WHERE assembly=%s AND resolution=%s AND chromosome=%s AND start>=%s AND start<=%s AND end>=%s AND end<=%s";
+        sql = "SELECT chromosome, start, end, dataset, experiment FROM tads WHERE assembly=%s AND resolution=%s AND chromosome=%s AND start>=%s AND start<=%s";
+        
+        """
+        For the moment I have put in a manual fudge factor allowing for the out
+        of range TADS
+        """
+        param = (accession_id, resolution, chr_id, start-10, end, start, end+10)
+        
+        rows = self.session.execute(sql, param)
+        
+        results = []
+        for row in rows:
+            results.append({"chr": str(row.chromosome), "start": row.start, "end": row.end, "dataset": str(row.dataset), "experiment": str(row.experiment), '_links': {}})
+        return {"results": results, "SQL": cursor._last_executed}
